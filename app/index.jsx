6f45c9f7af4688875150f1634ee1a758e0b6fe06
kami-christella <kamichristella@gmail.com>
2025-06-06 22:45:24 +0200

    making mobile application using react native

---
+++ app/index.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import Logo from '../assets/img/1.png'; // Adjust the path as necessary
+
+const Home =()=>{
+    return (
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.img} />
+        <Text style={styles.title}>The Number 1</Text>
+        <Text style={{marginTop: 10, marginBottom: 30, color:'red'}}>
+            Reading List App
+        </Text>
+        </View>
+    );
+}
+export default Home;
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title:{
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    img:{
+        marginVertical: 20,
+        boxShadow:'4px 4px',
+      width: 100,
+      height: 100
+    }
+});
