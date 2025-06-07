c1c5360807f7692f57163c8ade932b3af92b2416
kami-christella <kamichristella@gmail.com>
2025-06-07 08:14:43 +0200

    added slack to navigate to pages

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,10 @@
+import {  Stack } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 const RootLayout=()=>{
 return(
     <View style={{flex:1}}>
+        <Stack />
         <Text>Footer</Text>
     </View>
 )
