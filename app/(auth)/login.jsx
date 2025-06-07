ebe47836821e57f91f3209a8aca97081f088cc10
kami-christella <kamichristella@gmail.com>
2025-06-07 21:50:21 +0200

    added login button

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,16 +1,30 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Pressable, Text } from "react-native";
 import ThemedText from "../../components/ThemedText";
 import ThemedView from "../../components/ThemedView";
 import Spacer from "../../components/Spacer";
 import { Link } from "expo-router";
+import {Colors} from "../../constants/Colors";
 
 const Login =()=>{
+
+    const handleSubmit = () => {
+        // Handle login logic here
+        console.log("Login button pressed");
+    }
     return (
         <ThemedView style={styles.container}>
+
             <Spacer/>
             <ThemedText title={true} style={styles.title}>
                 Login To Your Account
              </ThemedText>
+             <Pressable
+             onPress={handleSubmit}
+              style={({pressed }) => [styles.btn, pressed && styles.pressed]}>
+                 <Text style={{ color: '#f2f2f2'}}>
+                     Login
+                 </Text>
+             </Pressable>
             <Spacer height={100}/>
             <Link href="/register">
                 <ThemedText style={{textAlign: 'center'}}>
@@ -37,5 +51,13 @@ const styles = StyleSheet.create({
     link: {
         marginVertical: 10,
         borderBottomWidth: 1
-    }
+    },
+    btn: {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5,
+    },
+    pressed: {
+        opacity: 0.8
+    },
 })
