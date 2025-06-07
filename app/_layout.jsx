cd54f715c05d0d8a781f9567b104fdd093a89249
kami-christella <kamichristella@gmail.com>
2025-06-07 08:58:45 +0200

    added styles in pages

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,8 +3,13 @@ import { StyleSheet, Text, View } from "react-native";
 
 const RootLayout=()=>{
 return(
-    
-   <Stack >
+
+   <Stack screenOptions={{
+     headerStyle: {
+       backgroundColor: '#ddd',
+       headerTintColor: '#333',
+     },
+   }}>
   <Stack.Screen name="index" options={{title:"Home"}}/>
   <Stack.Screen name="about" options={{title:"About Us"}}/>
   <Stack.Screen name="contact" options={{title:"Contact Us", headerShown: false}}/>
