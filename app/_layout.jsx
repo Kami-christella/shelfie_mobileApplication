8b9b1a7acd269d7c3d0b281b20cd20369dcd75f1
kami-christella <kamichristella@gmail.com>
2025-06-07 08:05:54 +0200

    added layout page

---
+++ app/_layout.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const RootLayout=()=>{
+return(
+    <View style={{flex:1}}>
+        <Text>Footer</Text>
+    </View>
+)
+}
+export default RootLayout;
+
+const styles=StyleSheet.create({});
