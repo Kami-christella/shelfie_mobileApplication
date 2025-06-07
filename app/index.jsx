c38602e9e382efe39fecf37425fb6c87e757df3f
kami-christella <kamichristella@gmail.com>
2025-06-07 16:10:43 +0200

    corrected errors that made dark theme not to work

--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#e0dfe8'
+    // backgroundColor: '#e0dfe8'
   },
   img: {
     marginVertical: 20,
