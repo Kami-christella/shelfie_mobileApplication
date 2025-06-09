3fa9944a88cdb246ae330dd451a017a25970ce32
kami-christella <kamichristella@gmail.com>
2025-06-09 12:08:18 +0200

    adde another way to use safe area

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,14 +1,29 @@
 import { useColorScheme, View } from 'react-native'
 import { Colors } from '../constants/Colors'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
+import { use } from 'react'
 
-const ThemedView = ({ style, ...props }) => {
+const ThemedView = ({ style, safe =false, ...props }) => {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme] ?? Colors.light
 
+  
+  if(!safe) 
   return (
-    <SafeAreaView 
+    <View 
       style={[{ backgroundColor: theme.background }, style]}
+      {...props}
+    />
+  )
+const insets=useSafeAreaInsets()
+
+  return(
+    <View
+      style={[{ backgroundColor: theme.background,
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+      },
+      style]}
       {...props}
     />
   )
