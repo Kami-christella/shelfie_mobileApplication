86f45b588f693a140e29a1c9b02f2d98a6c1f071
kami-christella <kamichristella@gmail.com>
2025-06-09 09:35:47 +0200

    added changes to layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,30 +1,3 @@
-// import { Stack } from "expo-router"
-// import { Colors } from "../constants/Colors"
-// import { useColorScheme } from "react-native"
-// import { StatusBar } from "expo-status-bar"
-
-// export default function RootLayout() {
-//   const colorScheme = useColorScheme()
-//   const theme = Colors[colorScheme] ?? Colors.light
-
-//   return (
-//     <>
-//       <StatusBar value="auto" />
-//       <Stack screenOptions={{
-//         headerStyle: { backgroundColor: theme.navBackground },
-//         headerTintColor: theme.title,
-//       }}>
-//         {/* Individual Screens */}
-//         <Stack.Screen name="index" options={{ title: "Home" }} />
-//         <Stack.Screen name="about" options={{ title: "About" }} />
-//         <Stack.Screen name="contact" options={{ title: "Contact" }} />
-//       </Stack>
-//     </>
-//   )
-// }
-
-
-
 import { Stack } from "expo-router"
 import { Colors } from "../constants/Colors"
 import { useColorScheme } from "react-native"
@@ -41,10 +14,40 @@ export default function RootLayout() {
         headerStyle: { backgroundColor: theme.navBackground },
         headerTintColor: theme.title,
       }}>
-       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        {/* Individual Screens */}
         <Stack.Screen name="index" options={{ title: "Home" }} />
-
+        <Stack.Screen name="about" options={{ title: "About" }} />
+        <Stack.Screen name="contact" options={{ title: "Contact" }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)/profile" options={{ title: "Profile" }} />
       </Stack>
     </>
   )
 }
+
+
+
+// import { Stack } from "expo-router"
+// import { Colors } from "../constants/Colors"
+// import { useColorScheme } from "react-native"
+// import { StatusBar } from "expo-status-bar"
+
+// export default function RootLayout() {
+//   const colorScheme = useColorScheme()
+//   const theme = Colors[colorScheme] ?? Colors.light
+
+//   return (
+//     <>
+//       <StatusBar value="auto" />
+//       <Stack screenOptions={{
+//         headerStyle: { backgroundColor: theme.navBackground },
+//         headerTintColor: theme.title,
+//       }}>
+//        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+//         <Stack.Screen name="index" options={{ title: "Home" }} />
+
+//       </Stack>
+//     </>
+//   )
+// }
