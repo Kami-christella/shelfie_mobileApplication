014509b0f3b363ccd17ed95adcd1aaa4e5fe3fd3
kami-christella <kamichristella@gmail.com>
2025-06-10 11:52:36 +0200

    added user provider in layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,13 +2,14 @@ import { Stack } from "expo-router"
 import { Colors } from "../constants/Colors"
 import { useColorScheme } from "react-native"
 import { StatusBar } from "expo-status-bar"
+import { UserProvider } from "../contexts/UserContext"
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme] ?? Colors.light
 
   return (
-    <>
+    <UserProvider>
       <StatusBar value="auto" />
       <Stack screenOptions={{
         headerStyle: { backgroundColor: theme.navBackground },
@@ -22,7 +23,7 @@ export default function RootLayout() {
         <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
         {/* <Stack.Screen name="(dashboard)/profile" options={{ title: "Profile" }} /> */}
       </Stack>
-    </>
+    </UserProvider>
   )
 }
 
