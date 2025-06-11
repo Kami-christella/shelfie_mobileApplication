cd1611d2053f7715a98d65de34911f38579ec11e
kami-christella <kamichristella@gmail.com>
2025-06-11 09:43:05 +0200

    removed error

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { useUser } from '../../hooks/useUser'
 import Spacer from "../../components/Spacer"
 import ThemedText from "../../components/ThemedText"
