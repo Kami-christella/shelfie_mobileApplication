5db50caabea147384b53d91f1c472320a1315fac
kami-christella <kamichristella@gmail.com>
2025-06-12 11:58:15 +0200

    displaying all books

--- components/ThemedCard.jsx
+++ components/ThemedCard.jsx
@@ -1,5 +1,7 @@
 import { StyleSheet, useColorScheme, View } from "react-native";
 
+import { Colors } from '../constants/Colors'
+
 const ThemedCard = ({ style, ...props }) => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,61 +1,163 @@
-import { createContext, useState } from "react";
-import { databases } from "../lib/appwrite";
-import { ID, Permission, Role } from "react-native-appwrite";
-import { useUser } from './../hooks/useUser';
+// import { createContext, useEffect, useState } from "react";
+// import { databases } from "../lib/appwrite";
+// import { ID, Permission, Query, Role } from "react-native-appwrite";
+// import { useUser } from './../hooks/useUser';
+
+// const DATABASE_ID="kamikazi"
+// const COLLECTION_ID="684a6d00002eab70c037"
+
+// export const BooksContext = createContext()
+
+// export function BooksProvider({ children}){
+//     const [books, setBooks] = useState([])
+//     const {user} = useUser()
+
+//     async function fetchBooks() {
+//         try {
+//        const response= await databases.listDocuments(
+//         DATABASE_ID,
+//         COLLECTION_ID,
+//         [
+//              Query.equal('userId', user.$id)
+//         ]
+//        )
+//        setBooks(response.documents)
+//        console.log(response.documents)
+//         } catch(error){
+//             console.log(error.message)
+//         }
+//     }
+//     async function fetchBookById(id) {
+//          try {
+
+//         } catch(error){
+//             console.log(error.message)
+//         }
+//     }
+
+//     async function createBook(data) {
+//          try {
+//           const newBook = await databases.createDocument(
+//             DATABASE_ID,
+//             COLLECTION_ID,
+//             ID.unique(),
+//             {...data, userId:user.$id},
+//             [
+//                 Permission.read(Role.user(user.$id)),
+//                 Permission.update(Role.user(user.$id)),
+//                 Permission.delete(Role.user(user.$id)),
+//             ]
+//           )
+//         } catch(error){
+//             console.log(error.message)
+//         }
+//     }
+//     async function deleteBook(id) {
+//          try {
+
+//         } catch(error){
+//             console.log(error.message)
+//         }
+//     }
+//     useEffect(()=>{
+//         if(user)
+//         {
+//             fetchBooks()
+//         } else{
+//             setBooks([])
+//         }
+
+//     },[user])
+//     return (
+//         <BooksContext.Provider 
+//           value={{books, fetchBooks,fetchBookById, createBook,deleteBook}}
+//         >
+//           {children}  
+//         </BooksContext.Provider>
+//     )
+// }
+
+
+import { createContext, useEffect, useState } from "react"
+import { databases } from "../lib/appwrite"
+import { ID, Permission, Query, Role } from "react-native-appwrite"
+import { useUser } from "../hooks/useUser"
 
 const DATABASE_ID="kamikazi"
 const COLLECTION_ID="684a6d00002eab70c037"
 
 export const BooksContext = createContext()
 
-export function BooksProvider({ children}){
-    const [books, setBooks] = useState([])
-    const {user} = useUser()
+export function BooksProvider({children}) {
+  const [books, setBooks] = useState([])
+  const { user } = useUser()
 
-    async function fetchBooks() {
-        try {
+  async function fetchBooks() {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID, 
+        COLLECTION_ID,
+        [
+          Query.equal('userId', user.$id)
+        ]
+      )
 
-        } catch(error){
-            console.log(error.message)
-        }
+      setBooks(response.documents)
+      console.log(response.documents)
+    } catch (error) {
+      console.error(error.message)
     }
-    async function fetchBookById(id) {
-         try {
+  }
 
-        } catch(error){
-            console.log(error.message)
-        }
+  async function fetchBookById(id) {
+    try {
+      
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  async function createBook(data) {
+    try {
+      await databases.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {...data, userId: user.$id},
+        [
+          Permission.read(Role.user(user.$id)),
+          Permission.update(Role.user(user.$id)),
+          Permission.delete(Role.user(user.$id)),
+        ]
+      )
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  async function deleteBook(id) {
+    try {
+      
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+
+    if (user) {
+      fetchBooks()
+    } else {
+      setBooks([])
     }
 
-    async function createBook(data) {
-         try {
-          const newBook = await databases.createDocument(
-            DATABASE_ID,
-            COLLECTION_ID,
-            ID.unique(),
-            {...data, userId:user.$id},
-            [
-                Permission.read(Role.user(user.$id)),
-                Permission.update(Role.user(user.$id)),
-                Permission.delete(Role.user(user.$id)),
-            ]
-          )
-        } catch(error){
-            console.log(error.message)
-        }
-    }
-    async function deleteBook(id) {
-         try {
+  }, [user])
 
-        } catch(error){
-            console.log(error.message)
-        }
-    }
-    return (
-        <BooksContext.Provider 
-          value={{books, fetchBooks,fetchBookById, createBook,deleteBook}}
-        >
-          {children}  
-        </BooksContext.Provider>
-    )
+  return (
+    <BooksContext.Provider 
+      value={{ books, fetchBooks, fetchBookById, createBook, deleteBook }}
+    >
+      {children}
+    </BooksContext.Provider>
+  )
 }
